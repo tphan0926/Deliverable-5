38d5454fefa0a7f08e8cc024bda04ebc74282e5f
wzhang4@umbc.edu <wzhang4@umbc.edu>
2026-05-07 18:42:58 -0400

    added some changes to index.html and server.js

---
+++ app/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Homepage route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
